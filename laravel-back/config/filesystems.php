bf3cf4c59d0051df73c7f0005282d62f1bab5646
Sara <dimossijanetsara@gmail.com>
2026-02-21 12:53:50 +0100

    Ajout du dump SQL et autres modifications

--- laravel-back/config/filesystems.php
+++ laravel-back/config/filesystems.php
@@ -38,14 +38,15 @@ return [
             'report' => false,
         ],
 
-        'public' => [
-            'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => rtrim(env('APP_URL'), '/').'/storage',
-            'visibility' => 'public',
-            'throw' => false,
-            'report' => false,
-        ],
+       'public' => [
+    'driver' => 'local',
+    'root' => storage_path('app/public'),
+    'url' => rtrim(env('APP_URL', ''), '/') . '/storage',
+    'visibility' => 'public',
+    'throw' => false,
+    'report' => false,
+      ],
+
 
         's3' => [
             'driver' => 's3',
